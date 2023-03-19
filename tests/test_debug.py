0d3ee23c85f2064efb5dfc6c792eeae74139caea
Puncher <65789180+Puncher1@users.noreply.github.com>
2023-03-19 15:49:18 +0100

    [tests]: Add error tests for log

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -2,6 +2,16 @@ from cogs.utils.debug import log, LogLevel
 
 
 log("This is an info", level=LogLevel.INFO, context="test:test_debug")
-log("This is a warning", level=LogLevel.WARNING, context="test:test_debug")
-log("This is an error", level=LogLevel.ERROR, context="test:test_debug")
-log("This is a critical error", level=LogLevel.CRITICAL, context="test:test_debug")
+log("This is a warning", level=LogLevel.WARNING)
+log("This is an error", level=LogLevel.ERROR)
+log("This is a critical error", level=LogLevel.CRITICAL)
+
+try:
+    log("This is a critical error", level=None)
+except Exception as e:
+    print(f"1\t{e.__class__.__name__}: {e}")
+
+try:
+    log("This is a critical error", level=LogLevel.CRITICAL, context=None)
+except Exception as e:
+    print(f"2\t{e.__class__.__name__}: {e}")
